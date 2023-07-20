994273cc667e85dcb9692eeb0bbe66979de7a8ac
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-07-19 23:03:54 -0300

    verificar

--- teste.php
+++ teste.php
@@ -5,16 +5,18 @@ use PaymentPhp\Model\TransferenciaBancaria;
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-//$pagamento = new TransferenciaBancaria("134");
-////    = new CartaoDeCredito("Emmanuel", "1234567890", "1R6","145", "12");
+$pg = new TransferenciaBancaria("134");
+var_dump($pg->validarDados());
+exit();
+//    = new CartaoDeCredito("Emmanuel", "1234567890", "1R6","145", "12");
 //print_r($pagamento);
 //print_r($pagamento->operacaoDepagamento());
 
 
-$codBarras = "001 9 337370000000100 05009 401448 16060680935031";
-$valorDoBoleto = "150";
-$valorDePagamanento = "150";
-$pg =  new Boleto($codBarras, $valorDoBoleto, $valorDePagamanento);
-
-exit();
-var_dump($pg->operacaoDepagamento());
+//$codBarras = "001 9 337370000000100 05009 401448 16060680935031";
+//$valorDoBoleto = "150";
+//$valorDePagamanento = "150";
+//$pg =  new Boleto($codBarras, $valorDoBoleto, $valorDePagamanento);
+//var_dump($pg->validarDados());
+//exit();
+//var_dump($pg->operacaoDepagamento());
